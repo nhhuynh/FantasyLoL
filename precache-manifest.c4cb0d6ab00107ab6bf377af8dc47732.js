914e802b3016b66badb5f67e5ff9a3774099c5e9
Nam Huynh <nhhuynh97@gmail.com>
2020-02-10 18:32:53 -0800

    Updates

---
+++ precache-manifest.c4cb0d6ab00107ab6bf377af8dc47732.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e112f6443b03ba8f2bd100d9afdaae4e",
+    "url": "/LoLFantasy/index.html"
+  },
+  {
+    "revision": "f2729841c8fbe2d8a1f8",
+    "url": "/LoLFantasy/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b9dca359b292023e7fda",
+    "url": "/LoLFantasy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f2729841c8fbe2d8a1f8",
+    "url": "/LoLFantasy/static/js/2.c79ddaab.chunk.js"
+  },
+  {
+    "revision": "5776768c082169f6074c75f93f8b9356",
+    "url": "/LoLFantasy/static/js/2.c79ddaab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9dca359b292023e7fda",
+    "url": "/LoLFantasy/static/js/main.33f2cda1.chunk.js"
+  },
+  {
+    "revision": "bdb61216b1d22a7b415b",
+    "url": "/LoLFantasy/static/js/runtime-main.500e72b3.js"
+  }
+]);
